c60b332e1e4cbc3d4940ce6c3b4fb47e29cf710b
Jordon Clarke <jc138701@terpmail.umd.edu>
2025-11-06 16:02:50 -0500

    Updated social_network script with new functions

--- social_network.py
+++ social_network.py
@@ -1,1 +1,16 @@
-print("hello world")
+Calling = "Hello world and random viewer"
+x = 2
+if x == 1:
+    print (Calling)
+
+else: 
+    print("Sorry guess I'm not saying hello today")
+
+def connect(person):
+    random_list = ("ryan", "jacob", "jaden", "tyreek", "ana", "rose")
+    if person is not random_list:
+        print(f"No luck looking for {person} bucko")
+
+connect(person="Zeke")
+
+print("Finished")
